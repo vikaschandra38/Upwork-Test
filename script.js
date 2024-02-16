68e4175f52fad87c8332ad301983d890365ed435
vikaschandra38 <vikaschandra38@gmail.com>
2024-02-16 09:19:52 +0530

    test

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 function openInstagramApp() {
 // Open Instagram
-// const url = "intent://instagram.com/#Intent;user?username=vikaschandra38;scheme=https;package=com.instagram.android;end";
-const url = 'instagram://user?username=exampleuser';
+const url = "intent://instagram.com/SplashScreen#Intent;user?username=vikaschandra38;scheme=https;package=com.instagram.android;end";
 window.location.replace(url); 
+}
+
+function openInstagramLink(){
+  // Create a new anchor element
+var link = document.createElement('a');
+
+// Set attributes for the anchor element
+link.href = 'instagram://user?username=exampleuser';
+link.innerText = '';
+link.target = '_blank'; // Opens the link in a new tab
+link.id = "instagramLink"
+
+// Append the anchor element to an existing DOM element
+document.body.appendChild(link);
+// Remove the anchor element after a certain time (e.g., 5 seconds)
+setTimeout(function() {
+  var elementToRemove = document.getElementById('instagramLink');
+  if (elementToRemove) {
+      elementToRemove.parentNode.removeChild(elementToRemove);
+  }
+}, 5000); 
 }
 
 const openAppButton = document.getElementById('openAppButton');
