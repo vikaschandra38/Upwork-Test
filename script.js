2eec5b3f2747a821edf4d699de4a8bf8420c545f
vikaschandra38 <vikaschandra38@gmail.com>
2024-02-16 08:56:36 +0530

    retest

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// function openInstagramApp() {
-// // Open Instagram
-// // const url = "intent://instagram.com/#Intent;user?username=vikaschandra38;scheme=https;package=com.instagram.android;end";
-// const url = 'instagram://user?username=exampleuser';
-// window.location.replace(url); 
-// }
+function openInstagramApp() {
+// Open Instagram
+// const url = "intent://instagram.com/#Intent;user?username=vikaschandra38;scheme=https;package=com.instagram.android;end";
+const url = 'instagram://user?username=exampleuser';
+window.location.replace(url); 
+}
 
-// const openAppButton = document.getElementById('openAppButton');
-// openAppButton.addEventListener('click', openInstagramApp);
+const openAppButton = document.getElementById('openAppButton');
+openAppButton.addEventListener('click', openInstagramApp);
