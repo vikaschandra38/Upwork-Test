10bb86fa15e2a32b86e023c5734e835a7282a4f9
vikaschandra38 <vikaschandra38@gmail.com>
2024-02-16 07:59:27 +0530

    instagram link

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function openInstagramApp() {
-    // if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-        window.location.href = 'http://play.google.com/store/apps/details?id=com.truecaller&hl=en';
-    // }
-    // if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
-    //     window.location.href = 'http://itunes.apple.com/lb/app/truecaller-caller-id-number/id448142450?mt=8';
-    // }
+// Open Instagram
+const url = "intent://instagram.com/#Intent;scheme=https;package=com.instagram.android;end";
+
+window.location.replace(url); 
 }
 
 const openAppButton = document.getElementById('openAppButton');
