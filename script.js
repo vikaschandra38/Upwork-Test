0c92775dd7f947333163df0077ec0ba706769bf5
vikaschandra38 <vikaschandra38@gmail.com>
2024-02-16 07:44:00 +0530

    test

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-async function openInstagramApp() {
-    // Check if Instagram app is installed
-    var userAgent = navigator.userAgent || window.opera;
-    
-    if (/android/i.test(userAgent)) {
-        // Android device
-        var timeout = 2000; // Timeout in milliseconds
-        var startTime = Date.now();
-
-        // Attempt to open Instagram app
-        var iframe = document.createElement('iframe');
-        iframe.style.display = 'none';
-        iframe.src = 'instagram://app';
-        document.body.appendChild(iframe);
-
-        setTimeout(function() {
-            var endTime = Date.now();
-            // If Instagram app does not open within the timeout, redirect to Google Play Store
-            if (endTime - startTime < timeout) {
-                window.location.href = 'https://play.google.com/store/apps/details?id=com.instagram.android';
-            }
-        }, timeout);
+function openInstagramApp() {
+  $(document).ready(function (){
+    if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
+        window.location.href = 'http://play.google.com/store/apps/details?id=com.truecaller&hl=en';
     }
+    if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
+        window.location.href = 'http://itunes.apple.com/lb/app/truecaller-caller-id-number/id448142450?mt=8';
+    }
+   });
 }
 
 const openAppButton = document.getElementById('openAppButton');
